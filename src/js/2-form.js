6a39625446a7ec5d6765365a0320873bbd46bddb
SerdiukSerhii <Serdyuk.logist@gmail.com>
2026-03-01 05:27:27 +0200

    add task-form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,36 @@
-console.log('Form');
+import '../css/2-form.css';
+
+const formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
+const savedData = localStorage.getItem(localStorageKey);
+
+if (savedData) {
+  const parsedData = JSON.parse(savedData);
+  formData.email = parsedData.email ?? '';
+  formData.message = parsedData.message ?? '';
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
+form.addEventListener('input', evt => {
+  formData[evt.target.name] = evt.target.value.trim();
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log(formData);
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+  formData.email = '';
+  formData.message = '';
+});
